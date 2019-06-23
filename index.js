8948cf727701e776aacc417388db86917142d4b1
Sandile Magagula <magagula.muzisandile@gmail.com>
2019-06-23 15:36:03 +0200

    Files updated

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ var cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.set('port', (process.env.PORT || 5000));
+const port = 5000;
 
-app.get('/', function(request, response) {
+app.set('port', (process.env.PORT || port));
+
+app.get('/', (request, response) => {
   response.send('Hello World!')
 });
 
-app.get('/users', function(req, res) {
+app.get('/users', (req, res) => {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
     {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
@@ -19,6 +21,6 @@ app.get('/users', function(req, res) {
   ]);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port')); 
+app.listen(app.get('port'), () => {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
